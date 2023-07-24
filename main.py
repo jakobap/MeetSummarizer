37ade2d7db057742fc8487a48c8dff825d39605b
jakobap <jakobpoerschmann@gmail.com>
2023-07-24 10:20:22 +0200

    webservice refactoring

--- main.py
+++ main.py
@@ -1,40 +1,16 @@
-import transcript
-import llm_prompting
+import os
 
-import datetime
+from flask import Flask
 
-def main(transcript_path: str):
-   # Create Transcript Object from Input
-   transcript_object = transcript.Transcript(transcript_path).load()
-
-   output = []
-   for count, prompt_chunk in enumerate(transcript_object.prompt_chunks):
-      output.append(f"##### Summarization Chunk {count} #####")
-      output.append(llm_prompting.text_model_api_call(prompt_chunk=prompt_chunk, attendees=transcript_object.attendees))
-
-   write_to_file(output, f"./output_{int(datetime.datetime.now().timestamp() * 1000)}.txt")
-
-   return 
-
-def write_to_file(list_of_strings, file_path):
-  """Writes a list of strings to a txt file.
-
-  Args:
-    list_of_strings: A list of strings to write to the file.
-    file_path: The path to the file to write to.
-  """
-
-  with open(file_path, "w") as f:
-    for string in list_of_strings:
-      f.write(string + "\n")
-    
-    return
+app = Flask(__name__)
 
 
-if __name__ == '__main__':
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
-#    main("./transcript_full.txt")
 
-   main("./transcript2_raw.txt")
-
-   print("Hello World!")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
